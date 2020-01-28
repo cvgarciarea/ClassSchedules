df67fb097f9dc515c93fcf02de4e25d4c670fa0e
Cristian García <cristian99garcia@gmail.com>
2020-01-28 19:38:10 -0300

    Creo componente para enlaces
    Un componente básico que permite crear hipervínculos dentro de un texto.

---
+++ src/components/hyperlink.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Linking,
+  TouchableOpacity,
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+import Utils from '../utils/utils';
+import State from '../utils/state';
+import Colors from '../utils/colors';
+
+export default class Hyperlink extends React.Component {
+
+  static defaultProps = {
+    title: null,
+    url: null,
+    onPress: null,
+  };
+
+  static propTypes = {
+    title: PropTypes.string,
+    url: PropTypes.string.isRequired,
+    onPress: PropTypes.func,
+  };
+
+  render() {
+    const theme = Colors.Themes[State.theme];
+
+    return (
+      <TouchableOpacity
+        onPress={ () => {
+          let {
+            url,
+            onPress,
+          } = this.props;
+
+          if (Utils.emptyValue(onPress)) {
+            Linking.openURL(url)
+          } else {
+            onPress(url);
+          }
+        }}
+      >
+        <Text
+          style={{
+            color: theme.link,
+            borderBottomWidth: 1,
+            borderColor: theme.link,
+          }}
+        >
+          { this.props.title || this.props.url }
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+}
